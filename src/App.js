5307a5ee88ff2b105ef5649870927d830f54606b
Hannah Roe <hannahdroe@gmail.com>
2018-09-24 18:57:13 -0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movies";
+import NavBar from "./components/navBar";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Customers from "./components/Customers";
+import Rentals from "./components/Rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
+
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
